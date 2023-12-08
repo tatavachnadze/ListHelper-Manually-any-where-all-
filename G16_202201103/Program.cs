41e239269fa37d8df503d8b919865f1c3db358fa
Tamar Vachnadze <tata0vachnadze@gmail.com>
2023-12-09 00:21:16 +0400

    Add project files.

---
+++ G16_202201103/Program.cs
@@ -0,0 +1,33 @@
+﻿using MyLinq;
+
+namespace G16_202201103
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 2, -11, -1, 7, 3 };
+            //var result = x.Where(n => n > 0);
+            var result = ListHelper.Where(x, n => n > 0);
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            var result1 = ListHelper.Any(x, n => n % 2 == 0);
+            Console.WriteLine(result1);
+
+            var result2 = ListHelper.All(x, n => n % 2 == 1 || n % 2 == -1);
+            Console.WriteLine(result2);
+        }
+        //Func<int, int, int> f = delegate (int x, int y)
+        //{
+        //    return x + y;
+        //};
+
+        //Func<int, int, int> f = (x, y) => x + y;
+
+        //Console.WriteLine(f(5, 2));
+    }
+}
+
---
+++ MyLinq/ListHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLinq
+{
+	public static class ListHelper
+	{
+		public static IEnumerable<T> Where<T>(IEnumerable<T> list, Func<T, bool> predicate)
+		{
+			ValidateParameters(list, predicate);
+
+			foreach (var item in list)
+			{
+				if (predicate(item))
+				{
+					yield return item;
+				}
+			}
+		}
+
+		public static bool Any<T>(IEnumerable<T> list, Func<T, bool> predicate)
+		{
+			ValidateParameters(list, predicate);
+
+			return Where(list, predicate).Count() != 0;
+		}
+
+		public static bool All<T>(IEnumerable<T> list, Func<T, bool> predicate)
+		{
+			ValidateParameters(list, predicate);
+
+			return Where(list, predicate).Count() == list.Count();
+		}
+
+		private static void ValidateParameters<T>(IEnumerable<T> list, Func<T, bool> predicate)
+		{
+			if (list == null) throw new ArgumentNullException(nameof(list));
+			if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+		}
+	}
+
+}
